9180e0e32252d12eeba186328ed790c9de8f724e
0x00rick <17012133+0x00rick@users.noreply.github.com>
2018-05-09 12:05:12 +0200

    Update IOTOS_installer.sh

--- IOTOS_installer.sh
+++ IOTOS_installer.sh
@@ -8,7 +8,7 @@ echo "██║╚██████╔╝   ██║██╗╚████�
 echo "╚═╝ ╚═════╝    ╚═╝╚═╝ ╚═════╝ ╚══════╝"
 
 
-LIST_OF_APPS="curl minicom binwalk git python-pip python3-pip python3 python3-dev build-essential automake autoconf libtool libcurl3 \
+LIST_OF_APPS=" libcurl4 curl minicom binwalk git python3 python3-dev python-pip python3-pip build-essential automake autoconf libtool \
     git unzip python-dev texinfo make pkg-config wireshark zsh curl wget zlib1g-dev liblzma-dev python-magic tcpdump \
     gdb gdb-multiarch unrar libssl-dev libx32gcc-4.8-dev libc6-dev-i386 binutils-arm-linux-gnueabi vim qt4-qmake\
     binutils-arm-linux-gnueabi libffi-dev libc6-powerpc-cross libc6-mipsel-cross binutils-powerpc-linux-gnu screen afl libfontconfig1 \
@@ -20,7 +20,6 @@ echo "---------------------------------------"
 echo "Installer starting...."
 echo "---------------------------------------"
 
-sudo apt-get autoremove -y
 sudo apt-get update -y
 sudo apt-get install $LIST_OF_APPS -y
 
@@ -31,22 +30,28 @@ sudo apt-get install libncurses5:i386 libc6-dbg:i386 libc6-i686:i386 libexpat1:i
  libice6:i386 libpcre3:i386 libsm6:i386 libstdc++6:i386 libuuid1:i386 libx11-6:i386 libxau6:i386 libxcb1:i386 libxdmcp6:i386 libxext6:i386 libxrender1:i386 zlib1g:i386 \
  libx11-xcb1:i386 libdbus-1-3:i386 libxi6:i386 libsm6:i386 libcurl3:i386 -y
 
-sudo pip3 install $PYTHON3_MODULES
-sudo pip install $PYTHON2_MODULES
+sudo pip3 -H install $PYTHON3_MODULES
+sudo pip -H install $PYTHON2_MODULES
 sudo pip install --upgrade pip
 sudo pip install --upgrade pwntools
 sudo pip3 install --upgrade pip
 
-
+sudo apt-get install curl cmake -y
 
 mkdir GIT && cd GIT
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo chsh -s $(which zsh)
+sudo apt-get install ttf-ancient-fonts fonts-powerline -y
+wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme && mv bullet-train.zsh-theme ~/.oh-my-zsh/themes/
+wget https://raw.githubusercontent.com/0x00rick/IOTOS/master/.zshrc && mv ~/
+
 wget http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run && chmod +x qt-unified-linux-x64-online.run && sudo ./qt-unified-linux-x64-online.run sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-wget https://raw.githubusercontent.com/0x00rick/IOTOS/master/install.py
+# wget https://raw.githubusercontent.com/0x00rick/IOTOS/master/install.py
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 wget https://download.jetbrains.com/python/pycharm-professional-2018.1.2.tar.gz &&  tar -zxvf pycharm-professional-2018.1.2.tar.gz -C ~/
-git clone https://github.com/fkie-cad/FACT_core.git && sudo cp install.py FACT_core/ && ./FACT_core/install.py
+# git clone https://github.com/fkie-cad/FACT_core.git && sudo cp install.py FACT_core/ && ./FACT_core/install.py
 git clone https://github.com/devttys0/baudrate.git
 git clone https://github.com/JonathanSalwan/ROPgadget.git && sudo python3 ROPgadget/setup.py install
 git clone https://github.com/radare/radare2.git && ./radare2/sys/install.sh
@@ -55,17 +60,21 @@ git clone https://github.com/rampageX/firmware-mod-kit.git
 git clone https://github.com/jondonas/linux-exploit-suggester-2.git les2
 git clone https://github.com/eblot/pyspiflash.git py_SPIFlash
 git clone https://github.com/pwndbg/pwndbg.git && cd pwndbg && ./setup.sh
-wget https://out7.hex-rays.com/files/idafree70_linux.run && chmod +x idafree70_linux.run && ./idafree70_linux.run..
+# wget https://out7.hex-rays.com/files/idafree70_linux.run && chmod +x idafree70_linux.run && ./idafree70_linux.run..
 git clone https://github.com/rc0r/afl-utils.git && cd afl-utils && sudo python setup.py install && cd ..
 echo "source /usr/local/lib/python2.7/dist-packages/exploitable-1.32_rcor-py2.7.egg/exploitable/exploitable.py" >> ~/.gdbinit
 git clone https://git.code.sf.net/p/openocd/code openocd-code && cd openocd-code && ./bootstrap && ./configure && make && sudo make install && cd ..
 wget https://cdn.ikalogic.com/dist/scanastudio/package/ScanaStudio.tar.bz2 && tar -xvjf ScanaStudio.tar.bz2 && ./ScanaStudio/install.sh
 sudo add-apt-repository universe
 git clone https://www.github.com/threat9/routersploit && cd routersploit && sudo python3 -m pip install -r requirements.txt
-sudo apt-get install libglib2.0-dev && sudo python3 -m pip install bluepy && cd ..
+sudo apt-get install libglib2.0-dev && sudo python3 -m pip install bluepy
 
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
 
-rm qt-opensource-linux-x64-5.10.1.run ScanaStudio.tar.bz2 pycharm-professional-2018.1.2.tar.gz idafree70_linux.run
 
 echo "---------------------------------------"
 echo "Installer finished...."
@@ -73,7 +82,7 @@ echo "Hopefully everything went okay...! ;)"
 echo "---------------------------------------"
 
 
-echo "alias rCutter='~/GIT/cutter/build/Cutter'" >> ~/.zshrc
+echo "alias rcutter='~/GIT/cutter/build/Cutter'" >> ~/.zshrc
 echo "alias pycharm='~/pycharm-2018.1.2/bin/pycharm.sh'" >> ~/.zshrc
 echo "alias scana='~/Ikalogic/ScanaStudio/ScanaStudio'" >> ~/.zshrc
 echo "alias ida='~/idafree-7.0/ida64'" >> ~/.zshrc
@@ -81,12 +90,17 @@ echo "alias rsf='python3 ~/GIT/routersploit/rsf.py'" >> ~/.zshrc
 echo "source ~/.zshrc"
 echo "-----------------------------------------------"
 echo "The following aliases were added to the .zshrc"
-echo "radare2 Cutter GUI --------------------> rCutter"
+echo "radare2 Cutter GUI --------------------> rcutter"
 echo "pycharm IDE ---------------------------> pycharm"
 echo "ScanaStudio ---------------------------> scana"
 echo "IDA Disassembler ----------------------> ida"
-echo "Routersploit ----------------------> rsf"
+echo "Routersploit --------------------------> rsf"
 echo "-----------------------------------------------"
+
+
+echo "---------------------------------------------------------------------"
+echo "!! Please logout and back in to make all changes work accordingly !!"
+echo "---------------------------------------------------------------------"
 
 
 
